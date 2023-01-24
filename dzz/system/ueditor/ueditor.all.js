5e85550da127dcd4401a9b73f04628c41df7d390
小胡 <3203164629@qq.com>
2023-01-24 16:49:35 +0800

    更新至V1.71，更新内容在笔记中查看

--- dzz/system/ueditor/ueditor.all.js
+++ dzz/system/ueditor/ueditor.all.js
@@ -12981,7 +12981,7 @@ UE.plugins['lineheight'] = function(){
 UE.plugins['insertcode'] = function() {
     var me = this;
     me.ready(function(){
-        utils.cssRule('pre','pre{margin:.5em 0;padding:.4em .6em;border-radius:8px;background:#f8f8f8;}',
+        utils.cssRule('pre','pre{margin:.5em 0;padding:.4em .6em;overflow: auto;border-radius:8px;background:#f8f8f8;}',
             me.document)
     });
     me.setOpt('insertcode',{
