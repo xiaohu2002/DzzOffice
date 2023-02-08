5f56f87d257934a3df099f742a081b070486bb0e
小胡 <3203164629@qq.com>
2023-02-08 12:58:46 +0000

    更新至V1.78
    Signed-off-by: 小胡 <3203164629@qq.com>

--- core/core_version.php
+++ core/core_version.php
@@ -14,7 +14,7 @@ if(!defined('CORE_VERSION')) {
 	define('CORE_VERSION', '2.02');
 	define('CORE_RELEASE', '20180909');
 	define('CORE_FIXBUG' , '20000000');
-	define('CORE_XHVERSION', '1.77');
+	define('CORE_XHVERSION', '1.78');
 	define('CORE_XHRELEASE', '20230207');
 	define('CORE_XHFIXBUG' , '20230207');
 }
