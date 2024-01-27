5e712bf0cd173d8cf53aafb572a9ec430d2a9fbc
小胡 <3203164629@qq.com>
2024-01-27 14:46:55 +0800

    更新至v1.87.1，更新内容在Dzzoffice笔记中查看

--- core/core_version.php
+++ core/core_version.php
@@ -14,7 +14,7 @@ if(!defined('CORE_VERSION')) {
 	define('CORE_VERSION', '2.02');
 	define('CORE_RELEASE', '20180909');
 	define('CORE_FIXBUG' , '20000000');
-	define('CORE_XHVERSION', '1.87');
-	define('CORE_XHRELEASE', '20240110');
+	define('CORE_XHVERSION', '1.87.1');
+	define('CORE_XHRELEASE', '20240127');
 	define('CORE_XHFIXBUG' , '20240110');
 }
