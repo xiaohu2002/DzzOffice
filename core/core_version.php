da084a8187234989fc7ed75a5ffba17b869d435a
小胡 <3203164629@qq.com>
2024-10-13 15:06:19 +0800

    修复分享的文件保存失败时无法显示提示信息问题和无法保存其他人分享的文件问题

--- core/core_version.php
+++ core/core_version.php
@@ -14,7 +14,7 @@ if(!defined('CORE_VERSION')) {
 	define('CORE_VERSION', '2.02');
 	define('CORE_RELEASE', '20180909');
 	define('CORE_FIXBUG' , '20000000');
-	define('CORE_XHVERSION', '1.90');
+	define('CORE_XHVERSION', '1.90.1');
 	define('CORE_XHRELEASE', '20241011');
-	define('CORE_XHFIXBUG' , '20241008');
+	define('CORE_XHFIXBUG' , '20241013');
 }
