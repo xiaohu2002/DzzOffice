953cf804fe272cbb80ff7f452d9548218344cfa5
小胡 <3203164629@qq.com>
2024-11-26 11:38:50 +0800

    更新

--- dzz/function/function_common.php
+++ dzz/function/function_common.php
@@ -16,23 +16,23 @@ if (CURMODULE) {
 	if ($_G['adminid']) return;
 	$appidxu = C::t('app_market')->fetch_by_identifier(CURMODULE);
 	if ($appidxu['appid']) {
-		if (!$appidxu['available']) return showmessage(lang('该应用已关闭，请联系管理员。'));
 		if ($appidxu['group'] == 0) return;
-		if ($appidxu['group'] == 3) showmessage(lang('您无权限使用该应用，请联系管理员为您分配权限。'));
 		if ($_G['uid']) {
 			$uid = $_G['uid'];
 		} elseif (isset($_GET['uidtoken'])) {
 			$uid = intval(dzzdecode($_GET['uidtoken']));
+		} elseif ($appidxu['group'] == -1) {
+			return;
 		}
 		if ($uid) {
+			if (!$appidxu['available']) showmessage(lang('该应用已关闭，请联系管理员。'));
+			if ($appidxu['group'] == 3) showmessage(lang('您无权限使用该应用，请联系管理员为您分配权限。'));
 			$apps = C::t('app_market')->fetch_all_by_default($uid,$onlyAppid = true);
 			$allowed = false;
 			$allowed = in_array($appidxu['appid'], $apps);
 			if (!$allowed) {
 				showmessage(lang('您无权限使用该应用，请联系管理员为您分配权限。'));
 			}
-		} elseif ($appidxu['group'] == -1) {
-			// 游客可以使用，跳过
 		} else {
 			dheader("Location: user.php?mod=login");
 		}
