b6b68c724a6c728a90ac8a6cec45bd80c79402b9
小胡 <3203164629@qq.com>
2024-12-17 17:08:00 +0800

    更新版本号，适配应用使用

--- core/core_version.php
+++ core/core_version.php
@@ -11,7 +11,7 @@ if(!defined('IN_DZZ')) {
 }
 
 if(!defined('CORE_VERSION')) {
-	define('CORE_VERSION', '2.3.0');
-	define('CORE_RELEASE', '20241115');
+	define('CORE_VERSION', '2.4.0');
+	define('CORE_RELEASE', '20241117');
 	define('CORE_FIXBUG' , '21000000');
 }
